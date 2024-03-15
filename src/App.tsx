980b10c60b16268207758499e5dc3647bc6c1f9c
Carlos Augusto <40805641+Cancastico@users.noreply.github.com>
2024-03-15 13:25:59 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { Router } from './routes/router';
+
+export default function App() {
+  return (
+    <div>
+      <NextUIProvider>
+        <Router />
+      </NextUIProvider>
+    </div>
+  )
+}

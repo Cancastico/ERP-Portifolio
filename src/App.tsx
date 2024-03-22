c8b7de7398f685929c21b0e6a8154cedcf484ac2
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-03-21 22:00:40 -0300

    SpeedInsights Implements

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { NextUIProvider } from "@nextui-org/react";
 import { Router } from "./routes/router";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function App() {
   return (
     <div>
+      <SpeedInsights/>
       <NextUIProvider>
         <Router />
       </NextUIProvider>
